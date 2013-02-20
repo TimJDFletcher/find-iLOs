a34ebee22e1f4f97593ed3b3313fda84e3e574a2
Rafael Casado Sanchez <rafacas@gmail.com>
2013-02-20 19:29:16 +0000

    Use mktemp for temporary files

--- find_iLOs.sh
+++ find_iLOs.sh
@@ -5,6 +5,8 @@
 
 network=$1
 
+ILOS_IPS=`mktemp /tmp/findilos.XXXXX`
+
 # Get a list of IPs with the 17988 TCP port opened (iLO Virtual Media port)
 # nmap options:
 #    -n: Never do DNS resolution.
@@ -13,12 +15,14 @@ network=$1
 #    -p 17988: only scans port 17988.
 #    -oG -: output scan in grepable format
 
-./test/nmap -n -sS -PN -p 17988 -oG - $network | grep /open/ | awk '{print $2}' > iLOs_ips
+./test/nmap -n -sS -PN -p 17988 -oG - $network | grep /open/ | awk '{print $2}' > $ILOS_IPS
 
-ips=($(<iLOs_ips));
+ips=($(<$ILOS_IPS));
 
 for i in "${ips[@]}"
 do
     echo $i
 done
 
+# Delete temporary files
+rm -f $ILOS_IPS
